c76ad704301f4e74010495648f2ac36987e637a3
Maria Gusova <masha.gu@gmail.com>
2020-08-08 21:04:55 +0200

    Add markdown example when app loads

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import ReactMarkdown from "react-markdown";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(example);
 
   return (
     <div className="App">
-      <h1>Markdown Previewer</h1>
+      <header>Markdown Previewer</header>
       <textarea
         id="editor"
         type="text"
         value={text}
         onChange={(event) => setText(event.target.value)}
       ></textarea>
-      <div id="preview">
+      <div id="preview" className="font">
         <ReactMarkdown source={text} escapeHtml={false} />
       </div>
     </div>
   );
 }
-
+const example =
+  "# Header 1 \n## Header 2 \n### Header 3 \n------------------------------------------------------------------------------------------\nHere is some *italic* and some __bold__ text!\n<li>list item 1</li><li>list item 2</li>";
 export default App;
