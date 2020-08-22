ebba034693e9f83909244b2d9580eb1c7b115e8c
Maria Gusova <masha.gu@gmail.com>
2020-08-22 19:26:07 +0200

    Updates

---
+++ precache-manifest.6d9de0f9887264da16a1e2b35a1fa472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0407089fbd3e04e9deccd4d360aee15b",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "efc254397f5a95dea127",
+    "url": "/markdown-previewer/static/css/main.dd0c4069.chunk.css"
+  },
+  {
+    "revision": "60d54231e24345059b4f",
+    "url": "/markdown-previewer/static/js/2.36bd13a3.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-previewer/static/js/2.36bd13a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc254397f5a95dea127",
+    "url": "/markdown-previewer/static/js/main.095b959f.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
